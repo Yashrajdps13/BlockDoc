4dad3edd24010c8c09517280ee2068a4a8e5af7f
Divyansh <srivas.divyansh22@gmail.com>
2024-02-06 17:02:23 +0530

    Contract is made and deployed successfully

---
+++ Blockchain/contracts/GDrive.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+contract GDrive {
+
+    struct Access {
+        address user;
+        bool access;
+    }
+
+    mapping (address => mapping(address => bool)) previous;
+    mapping (address => string[]) val;
+    mapping (address => Access[]) accessL;
+    mapping (address => mapping(address => bool)) owner;
+
+    function display(address u) external view returns(string[] memory) {
+        require(u == msg.sender || owner[u][msg.sender], "You don't have access");
+        return val[u];
+    }
+
+    function allow(address u) external {
+        owner[msg.sender][u] = true;
+        if (previous[msg.sender][u]){
+            for (uint i = 0;i < accessL[msg.sender].length;i++){
+                if (accessL[msg.sender][i].user == u) accessL[msg.sender][i].access = true;
+            }
+        }
+        accessL[msg.sender].push(Access(u, true));
+    }
+
+    function disallow(address u) public {
+        owner[msg.sender][u] = false;
+        for (uint i = 0;i < accessL[msg.sender].length;i++){
+            if (accessL[msg.sender][i].user == u) accessL[msg.sender][i].access = false;
+        }
+    }
+
+    function accessShare() public view returns(Access[] memory) {
+        return accessL[msg.sender];
+    }
+
+    function add(address u, string memory link) external {
+        val[u].push(link);
+    }
+}
---
+++ Blockchain/scripts/deploy.js
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const gdrive = await hre.ethers.deployContract('GDrive');
+
+  await gdrive.waitForDeployment();
+
+  console.log("Token Address: ", gdrive.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+//0x3192554A56f00798228F14a667024d31D941207D
